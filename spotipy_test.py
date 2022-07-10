79a3a151e810b945dcdebc6addcbca14b17d7f34
brettadrianfox <brettfox777@outlook.com>
2022-07-10 16:13:45 -0700

    Basic playlist switcher is now functional!

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,7 +1,8 @@
 import os
 import re
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
 def format_playname(playname_raw, playlist, pattern_2):
     playname_short = playname_raw.group()
@@ -17,14 +18,10 @@ def reformat_playname(playlist_dict, playlist, playname_tuple):
         playname_short = playname_tuple[0] + playname_short_end
     else:
         playname_short = playname_tuple[0]
-    playlist_dict[playlist['uri']] = playname_short
+    playlist_dict[playname_short] = playlist['uri']
     return playlist_dict
 
-def main():
-    auth_manager = SpotifyClientCredentials(client_id=os.environ.get("SPOTIPY_CLIENT_ID"), client_secret=os.environ.get("SPOTIPY_CLIENT_SECRET"))
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    playlists = sp.user_playlists(os.environ.get("SPOTIFY_USERNAME"))
+def init_playlist_dict(sp: spotipy.Spotify, playlists: spotipy.Spotify.user_playlists):
     playlist_dict = {}
     while playlists:
         for i, playlist in enumerate(playlists['items']):
@@ -34,38 +31,58 @@ def main():
             playname_raw = re.search(pattern_1, playlist['name']) # Capturing the word(s) after the colon
             if playname_raw:
                 playname_tuple = format_playname(playname_raw, playlist, pattern_2)
-                playlist_dict = reformat_playname(playlist_dict, playlist, playname_tuple)
-                print(playlist['name']) #
-                print(list(playlist_dict)[-1]) #
+                playlist_dict = reformat_playname(playlist_dict, playlist, playname_tuple) # TODO: INQUIRE INTO "nomadc". IT SHOULD BE "nomad"
+                # print(playlist['name']) #
+                # print(list(playlist_dict)[-1]) #
                 # print("\n") #
             # else: #
                 # print("N/A") #
         if playlists['next']:
             playlists = sp.next(playlists)
         else:
-            print(playlist_dict.values()) #
+            # print(playlist_dict.values()) #
             playlists = sp.user_playlists(os.environ.get("SPOTIFY_USERNAME"))
-            break
+            return playlist_dict
     # Listing all D&D playlists with their URIs
-    # Also listing their shortened versions in playlist_dict
+    # Also listing their shortened versions in playlist_dict    
+
+
+def main():
+    token = SpotifyOAuth(client_id=os.environ.get("SPOTIPY_SWITCHER_CLIENT_ID"), client_secret=os.environ.get("SPOTIPY_SWITCHER_CLIENT_SECRET"), redirect_uri=os.environ.get("SPOTIPY_REDIRECT_URI"), scope="streaming,user-read-playback-state", username=os.environ.get("SPOTIFY_USERNAME"))
+    sp = spotipy.Spotify(auth_manager=token)
+
+    playlists = sp.user_playlists(os.environ.get("SPOTIFY_USERNAME"))
+    playlist_dict = init_playlist_dict(sp, playlists)
 
     on = True
     while on:
         user_input = input("Input a playlist name (type q to quit): ")
-        #user_input = "Location: Town/Village" #TEMP
-        if user_input == "q":
+        # user_input = "p" #TEMP
+        if user_input == "q" or user_input == "quit":
             on = False
-        while playlists:
-            for i, playlist in enumerate(playlists['items']):
-                if user_input == playlist['name']: # TEMP
-                    print(f'"{user_input}" is a playlist! Nice!') #
-                    print(playlist_dict.values()) #TEMP
-                    return
-            if playlists['next']:
-                playlists = sp.next(playlists)
-            else:
-                print(f'"{user_input}" is not a playlist!') #
-                playlists = None
+            return
+        # elif user_input == "p" or user_input == "pause": # TODO: IMPLEMENT PAUSE FUNCTIONALITY -> NEED TO CHECK IF SPOTIFY IS PLAYING TO AVOID ERROR
+            # sp.pause_playback()
+        elif user_input == "ls" or user_input == "list":
+            pprint(playlist_dict) # TODO: REFORMAT THE DICT THAT CONTAINS THE PLAYLIST NAMES. MAYBE ADD IT TO THE OLD PLAYLIST DICT THAT SPOTIPY GIVES US?
+        else:
+            while playlists: # TODO: REFORMAT THIS BLOCK
+                found_playlist = False # Variable that checks if we have found our variable
+                for key, val in playlist_dict.items():
+                    if user_input == key: # TEMP
+                        print(f'\n"{user_input}" is a playlist!\n') #
+                        sp.shuffle(True)
+                        sp.start_playback(context_uri=val)
+                        # print(playlist_dict.values()) #TEMP
+                        found_playlist = True
+                        break
+                if found_playlist == True:
+                    break
+                elif playlists['next']:
+                    playlists = sp.next(playlists)
+                else:
+                    print(f'\n"{user_input}" is not a playlist!\n') #
+                    break
     
 
 
