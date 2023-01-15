b6d211f787ebd19af936494ef12baf2a3e709e9b
brettadrianfox <brettfox777@outlook.com>
2023-01-15 13:02:25 -0800

    Adding throwaway scripts to gitignore

--- cmdnd_logic/json_to_list.py
+++ cmdnd_logic/json_to_list.py
@@ -4,5 +4,6 @@ This throwaway script generates a python list out of the json file for the SRD f
 
 import json
 
-with open("srd_5e_monsters.json") as srd_list:
+with open("srd_5e_monsters.json", encoding="utf-8") as srd_list:
+    # TODO: Check if srd_5e_monsters.json is really utf-8 on both Windows and Linux
     srd_list = json.load(srd_list)
