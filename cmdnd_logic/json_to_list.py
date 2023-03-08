695ad432f059187f019fd41d31f777b802251766
brettadrianfox <brettfox777@outlook.com>
2023-03-07 16:23:33 -0800

    Removing subdirectories!

--- cmdnd_logic/json_to_list.py
+++
@@ -1,9 +0,0 @@
-"""
-This throwaway script generates a python list out of the json file for the SRD for DND 5e monsters
-"""
-
-import json
-
-with open("srd_5e_monsters.json", encoding="utf-8") as srd_list:
-    # TODO: Check if srd_5e_monsters.json is really utf-8 on both Windows and Linux
-    srd_list = json.load(srd_list)
