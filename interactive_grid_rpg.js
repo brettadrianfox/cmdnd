af40a7f439b907a74fd21e6a557c1d27aa065981
b3lla-c0rtez <103864274+b3lla-c0rtez@users.noreply.github.com>
2025-01-17 12:39:27 -0800

    Update interactive_grid_rpg.js

--- interactive_grid_rpg.js
+++ interactive_grid_rpg.js
@@ -1,11 +1,8 @@
 const gridHeight = document.getElementById('inputHeight');
 const gridWidth = document.getElementById('inputWidth');
+
 let height = gridHeight.value;
 let width = gridWidth.value;
-const table = document.getElementById('pixelCanvas');
-const form = document.querySelector('form');
-const picker = document.getElementById('colorPicker');
-let color = picker.value;
 
 gridHeight.addEventListener("input", function() {
   height = document.getElementById('inputHeight').value;
@@ -15,10 +12,11 @@ gridWidth.addEventListener("input", function() {
   width = document.getElementById('inputWidth').value;
 })
 
+const table = document.getElementById('pixelCanvas');
+
 function createCanvas(event) {
   event.preventDefault();
-
-  table.innerHTML = ''
+  table.innerHTML = '';
   for (let h = 1; h <= height; h++) {
     const row = document.createElement('tr');
 
@@ -26,19 +24,27 @@ function createCanvas(event) {
       const cell = document.createElement('td');
       row.appendChild(cell);
     }
-
     table.appendChild(row);
   }
 }
 
+const form = document.querySelector('form');
+
+// bind createCanvas() to "submit"
 form.addEventListener('submit', createCanvas);
+
+// event listener to update color
+const picker = document.getElementById('colorPicker')
+let color = picker.value;
 
 picker.onchange = function() {
   color = this.value;
 }
 
+// function activated when user click on only
+
 function respondToClick(event) {
-  if (event.target.nodeName.toLowerCase() === 'td' {
+  if (event.target.nodeName.toLowerCase() === 'td') {
     event.target.style.backgroundColor = color;
   }
 }
